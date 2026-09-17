5fafed2840fa30021152ddcb02eccc9fa6d47bab
shohruxrustamov1129-debug <shohruxrustamov1129@gmail.com>
2026-09-17 17:38:33 +0500

    Update python.py

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ hello evernoye!
 
 kilogramm=int(int("son kiriting"))
 tonna=kilogramm//1000
-print(f"{kilogramm} kilogramm bu {tonna} shuncha tonna
+print(f"{kilogramm} kilogramm bu {tonna} shuncha tonna")
 
 # bu yerda nima bulyapti biz o'zgaruvchi ochib oldik ya'ni variable degan va uning ichiga butun son integer soldik int ni soldik va foydalanuvchi dan son olib hiloblashi uchun input dan foydalandik va pastda ya'na o'garuvchi variable ochib oldik va uni nomini tonna didik va kilogramm ni ichiga soldik foydalanuvchi raqam kiritsa 1000 ga bo'linishi uchun // bu esa butun bo'lish uchun ishlattik
-va print(f"{kilogramm}  qilib boshlaganimizning sababi biz ekranga chiqarish uchun foydalandik. Bu yerda f harfi formatlangan string degani u o'zgaruvchi ichidagi narsani olib chiqishga yordam beradi 
+# va print(f"{kilogramm}  qilib boshlaganimizning sababi biz ekranga chiqarish uchun foydalandik. Bu yerda f harfi formatlangan string degani u o'zgaruvchi ichidagi narsani olib chiqishga yordam beradi 
